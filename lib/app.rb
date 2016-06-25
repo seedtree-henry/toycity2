ee83594eb4fd98a68fbe2a4731714360827e2812
SangHyuk KIM <HenryKIM@SangHyuks-iMac-24.local>
2016-06-26 02:26:59 +1000

    today's date and ascii arts

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,54 @@
 require 'json'
-path = File.join(File.dirname(__FILE__), '../data/products.json')
-file = File.read(path)
-products_hash = JSON.parse(file)
+require 'date'
+
+def start
+  setup_files # load, read, parse and create the files
+  create_report # create the report!
+end
+
+def setup_files
+  path = File.join(File.dirname(__FILE__), '../data/products.json')
+  file = File.read(path)
+  $products_hash = JSON.parse(file)
+  $report_file = File.new("report.txt", "w+")
+end
+
+def create_report
+  asc_sales
+  print_today
+  asc_products  
+end
 
 # Print "Sales Report" in ascii art
+def asc_sales
+  puts "  ____        _             ____                       _  " 
+  puts " / ___|  __ _| | ___  ___  |  _ \\ ___ _ __   ___  _ __| |_ "
+  puts " \\___ \\ / _` | |/ _ \\/ __| | |_) / _ \\ '_ \\ / _ \\| '__| __| "
+  puts "  ___) | (_| | |  __/\\__ \\ |  _ <  __/ |_) | (_) | |  | |_ "
+  puts " |____/ \\__,_|_|\\___||___/ |_| \\_\\___| .__/ \\___/|_|   \\__|"
+  puts "                                     |_|                   "
+end
 
 # Print today's date
+def print_today
+  date = DateTime.now
+  day = date.day
+  month = date.month
+  year = date.year
+  puts "Date : #{day}/#{month}/#{year}"
+end
 
 # Print "Products" in ascii art
+def asc_products
+  puts "                     _            _       "
+  puts "                    | |          | |      "
+  puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+  puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+  puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+  puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+  puts "| |                                       "
+  puts "|_|                                       "
+end
 
 # For each product in the data set:
 	# Print the name of the toy
@@ -18,9 +59,19 @@ products_hash = JSON.parse(file)
 	# Calculate and print the average discount (% or $) based off the average sales price
 
 # Print "Brands" in ascii art
-
+def asc_brands
+  puts " _                         _     "
+  puts "| |                       | |    "
+  puts "| |__  _ __ __ _ _ __   __| |___ "
+  puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+  puts "| |_) | | | (_| | | | | (_| \\__ \\"
+  puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+  puts ""
+end
 # For each brand in the data set:
 	# Print the name of the brand
 	# Count and print the number of the brand's toys we stock
 	# Calculate and print the average price of the brand's toys
 	# Calculate and print the total sales volume of all the brand's toys combined
+
+create_report
